c0afc694d78980891866e5afbdd7567d06eddf7e
Shchepin Daniel <shchepin.man@gmail.com>
2023-08-20 13:39:02 +0300

    uuid and functional components

--- src/App.js
+++ src/App.js
@@ -1,97 +1,98 @@
-import { Component } from "react";
+import React, { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import "./App.scss";
 import Task from "./components/Task/Task";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tasks: [
-        { id: 1, title: "Buy milk", completed: false },
-        { id: 2, title: "Make coffee", completed: false },
-        { id: 3, title: "Walking the dog in the park", completed: false },
-      ],
-      newTaskTitle: "",
-    };
+function App() {
+  const [tasks, setTasks] = useState([
+    { id: 1, title: "Buy milk", completed: false },
+    { id: 2, title: "Make coffee", completed: false },
+    { id: 3, title: "Walking the dog in the park", completed: false },
+  ]);
+  const [universities, setUniversities] = useState("");
+  const [newTaskTitle, setNewTaskTitle] = useState("");
 
-    this.handleDeleteTask = this.handleDeleteTask.bind(this);
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.addTask = this.addTask.bind(this);
-    this.clearInput = this.clearInput.bind(this);
+  useEffect(() => {
+    fetch(`http://universities.hipolabs.com/search?country=Ukraine`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => setUniversities(data.slice(0, 10)));
+  }, []);
+
+  function handleDeleteTask(taskId) {
+    setTasks(tasks.filter((task) => task.id !== taskId));
   }
 
-  handleDeleteTask = (taskId) => {
-    this.setState((prevState) => ({
-      tasks: prevState.tasks.filter((task) => task.id !== taskId),
-    }));
-  };
+  function handleInputChange(event) {
+    setNewTaskTitle(event.target.value);
+  }
 
-  handleInputChange = (event) => {
-    this.setState({ newTaskTitle: event.target.value });
-  };
-
-  addTask() {
-    const { newTaskTitle, tasks } = this.state;
-
+  function addTask() {
     if (newTaskTitle.trim() !== "") {
-      const newTask = {
-        id: tasks.length + 1,
-        title: newTaskTitle,
-        completed: false,
-      };
-
-      this.setState({
-        tasks: [...tasks, newTask],
-        newTaskTitle: "",
-      });
+      setTasks(
+        tasks.concat([{ id: uuidv4(), title: newTaskTitle, completed: false }])
+      );
     }
   }
 
-  clearInput() {
-    this.setState({
-      newTaskTitle: "",
-    });
+  function clearInput() {
+    setNewTaskTitle("");
   }
 
-  render() {
-    const { tasks, newTaskTitle } = this.state;
+  return (
+    <div className="App">
+      <h1>Todo App</h1>
 
-    return (
-      <div className="App">
-        <h1>Todo App</h1>
-
-        <div className="app-container">
-          <div className="tasks-input">
-            <input
-              value={newTaskTitle}
-              onChange={this.handleInputChange}
-              type="text"
-              className="newTask-input"
-            />
-            <div className="button-container">
-              <button onClick={this.addTask}>&#43;</button>
-              <button onClick={this.clearInput}>&times;</button>
-            </div>
+      <div className="app-container">
+        <div className="tasks-input">
+          <input
+            value={newTaskTitle}
+            onChange={handleInputChange}
+            type="text"
+            className="newTask-input"
+          />
+          <div className="button-container">
+            <button onClick={() => clearInput()}>&times;</button>
           </div>
+          <button className="addTask-button" onClick={() => addTask()}>
+            &#43;
+          </button>
+        </div>
 
-          <div className="tasks-container">
-            {tasks &&
-              tasks.map((task) => {
-                return (
-                  <Task
-                    id={task.id}
-                    title={task.title}
-                    isComplete={task.completed}
-                    handleDeleteTask={this.handleDeleteTask}
-                    key={task.id}
-                  />
-                );
-              })}
-          </div>
+        <div className="tasks-container">
+          {tasks &&
+            tasks.map((task) => {
+              return (
+                <Task
+                  id={task.id}
+                  title={task.title}
+                  isComplete={task.completed}
+                  handleDeleteTask={() => handleDeleteTask(task.id)}
+                  key={`task${task.id}`}
+                />
+              );
+            })}
+        </div>
+
+        <div className="universities">
+          {universities &&
+            universities.map((uni) => {
+              return (
+                <div>
+                  <div key={`uni${uni.name}`}>{uni.name}</div>
+                  <div>
+                    {uni.domains.map((domain) => { 
+                      return (<a href={`http://${domain}`}>{domain}</a>)
+                    })}
+                  </div>
+                </div>
+              );
+            })}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
